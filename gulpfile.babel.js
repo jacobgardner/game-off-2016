328dcb4a1d6c834b379784b033d0fd413abdafdd
Jacob Gardner <jacob.v.gardner@gmail.com>
2016-11-03 17:38:30 -0500

    Add css files, and watches for css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,9 +7,16 @@ import buffer from 'vinyl-buffer';
 import sourcemaps from 'gulp-sourcemaps';
 import browserSyncCreate from 'browser-sync';
 
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import postcssImport from 'postcss-import';
+
+const postcssProcessors = [postcssImport, autoprefixer];
+
 const browserSync = browserSyncCreate.create();
 
 const SOURCE_FILES = './src/**/*.js';
+const CSS_FILES = './css/**/*.css';
 
 gulp.task('js', () => {
     const b = browserify({
@@ -23,17 +30,29 @@ gulp.task('js', () => {
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('css', () => {
+    return gulp.src('./css/main.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(postcssProcessors))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js-watch', ['js'], () => {
     gulp.watch(SOURCE_FILES, ['js', () => {
-        console.log('watchingd');
         browserSync.reload();
     }]);
 });
 
-gulp.task('watch', ['js-watch']);
+gulp.task('css-watch', ['css'], () => {
+    gulp.watch(CSS_FILES, ['css']);
+});
+
+gulp.task('watch', ['js-watch', 'css-watch']);
 
 gulp.task('serve', () => {
     browserSync.init({

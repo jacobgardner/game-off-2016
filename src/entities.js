7242d99adb707acc23e24076a978a990024cea2e
Adam Lucz <adamlucz90@gmail.com>
2016-11-09 17:02:00 -0600

    entities added

--- src/entities.js
+++ src/entities.js
@@ -1,1 +1,9 @@
-entities.js
+class platformEntity {
+	//physicsBody will go here
+
+	draw(){
+
+        this.ctx.fillStyle = 'rgba(255,255,255, 0.9)';
+        this.ctx.fillRect(0, 100 , this.width, 25);
+	}
+}

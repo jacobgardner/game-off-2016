0903e3a95a72b8cd99c79a89eefa6e196bfc4d56
Jacob Gardner <jacob.v.gardner@gmail.com>
2016-11-09 15:44:54 -0600

    Add linting to gulp watching.  npm run publish, publishes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ import buffer from 'vinyl-buffer';
 import sourcemaps from 'gulp-sourcemaps';
 import browserSyncCreate from 'browser-sync';
 import ghPages from 'gulp-gh-pages';
+import eslint from 'gulp-eslint';
+import stylelint from 'gulp-stylelint';
 
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
@@ -19,7 +21,7 @@ const browserSync = browserSyncCreate.create();
 const SOURCE_FILES = './src/**/*.js';
 const CSS_FILES = './css/**/*.css';
 
-gulp.task('js', () => {
+gulp.task('js', ['js-lint'], () => {
     const b = browserify({
         entries: './src/main.js',
         debug: true,
@@ -34,7 +36,19 @@ gulp.task('js', () => {
         .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('css', () => {
+gulp.task('js-watch', ['js'], () => {
+    gulp.watch(SOURCE_FILES, ['js', () => {
+        browserSync.reload();
+    }]);
+});
+
+gulp.task('js-lint', () => {
+    gulp.src(SOURCE_FILES)
+        .pipe(eslint())
+        .pipe(eslint.format());
+});
+
+gulp.task('css', ['css-lint'], () => {
     return gulp.src('./css/main.css')
         .pipe(sourcemaps.init())
         .pipe(postcss(postcssProcessors))
@@ -43,14 +57,18 @@ gulp.task('css', () => {
         .pipe(browserSync.stream());
 });
 
-gulp.task('js-watch', ['js'], () => {
-    gulp.watch(SOURCE_FILES, ['js', () => {
-        browserSync.reload();
-    }]);
-});
-
 gulp.task('css-watch', ['css'], () => {
     gulp.watch(CSS_FILES, ['css']);
+});
+
+gulp.task('css-lint', () => {
+    gulp.src(CSS_FILES)
+        .pipe(stylelint({
+            failAfterError: false,
+            reporters: [
+                {formatter: 'string', console: true},
+            ],
+        }));
 });
 
 gulp.task('watch', ['js-watch', 'css-watch']);

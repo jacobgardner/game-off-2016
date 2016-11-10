3ea21b6ab46bd5a72d8392815fabdca2ae9905c1
Adam Lucz <adamlucz90@gmail.com>
2016-11-09 18:58:20 -0600

    physicsbody example

--- src/entities.js
+++ src/entities.js
@@ -1,8 +1,16 @@
-class platformEntity {
+import PhysicsBody from './physics_body.js';
+import {Rect} from './shapes.js';
+
+export default class platformEntity {
 	//physicsBody will go here
+	class PhysicsBody {
+        this.shape = Rect;
+        this.position = position;
+        this.velocity = velocity;
+        this.accel = accel;
+    }
 
-	draw(){
-
+	draw(ctx){
         this.ctx.fillStyle = 'rgba(255,255,255, 0.9)';
         this.ctx.fillRect(0, 100 , this.width, 25);
 	}

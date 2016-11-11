d425fe3c7bcb4c67d1826575f7954e01bc6641fa
jrhii <james.r.harter.ii@gmail.com>
2016-11-10 19:10:30 -0600

    Incomplete, saving for remote work

--- src/physics_body.js
+++ src/physics_body.js
@@ -3,11 +3,14 @@ import Polygon from 'polygon';
 
 export default class PhysicsBody {
     constructor(polygon, position, velocity, accel) {
-        this.polygon = polygon;
-        this.position = position; //[x,y] origin bottom left
-        this.velocity = velocity;//[units/s on x-axis, units/s on y-axis]
+        this.position = position.slice(); //[x,y] origin bottom left
+        this.velocity = velocity.slice();//[units/s on x-axis, units/s on y-axis]
         this.accel = accel;// m/(s^2)
         this.inView = false;
+
+        const center = polygon.center.toArray();
+        
+        this.polygon.translate(Vec2(this.position));
     }
 
     get x() {

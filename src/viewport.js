eb1ab1ca3aaeca39ec429db83a2ca598a435726f
Jacob Gardner <jacob.v.gardner@gmail.com>
2016-11-12 12:45:57 -0600

    Initial viewport work

--- src/viewport.js
+++ src/viewport.js
@@ -1,20 +1,46 @@
 import { findAppropriateWidth } from './utils';
+import AABB from './aabb';
+import Vec2 from 'victor';
 
 export default class Viewport {
     /**
-     * @param  {Vec2} origin      Should probably be the initial position of the player.
-     * @param  {Number} minHeight [description]
-     * @param  {Number} maxHeight [description]
+     * [constructor description]
+     * @param  {Vec2} center      Center of the viewport
+     * @param  {Number} minHeight   Initial height (and minHeight) of the viewport
+     * @param  {Number} buffer      The buffer area around the player to push the viewport around
      */
-    constructor(origin, minHeight, maxHeight) {
-        this.minHeight = minHeight;
-        this.maxHeight = maxHeight;
-        this.currentHeight = minHeight;
-
-        this.origin = origin.clone();
+    constructor(center, minHeight, buffer=1) {
+        const width = findAppropriateWidth(minHeight);
+        this.aabb = new AABB(Vec2(center.x - width / 2, center.y - minHeight / 2), Vec2(center.x + width / 2, center.y + minHeight / 2));
+        this.buffer = buffer;
     }
 
-    updateViewport(priorityEntities) {
+    /**
+     * This updates the viewport position based on the previous size
+     *     and the position of the player
+     * @param  {PhysicsBody} physicsBody The player's physicsBody
+     */
+    updateViewport(physicsBody) {
+        const {
+            upperRight,
+            lowerLeft,
+        } = physicsBody.aabb;
 
+        const push = Vec2(0, 0);
+
+
+        if (upperRight.x + this.buffer > this.aabb.upperRight.x) {
+            push.x = (upperRight.x + this.buffer) - this.aabb.upperRight.x;
+        } else if (lowerLeft.x - this.buffer < this.aabb.lowerLeft.x) {
+            push.x = (lowerLeft.x - this.buffer) - this.aabb.lowerLeft.x;
+        }
+
+        if (upperRight.y + this.buffer > this.aabb.upperRight.y) {
+            push.y = (upperRight.y + this.buffer) - this.aabb.upperRight.y;
+        } else if (lowerLeft.y - this.buffer < this.aabb.lowerLeft.y) {
+            push.y = (lowerLeft.y - this.buffer) - this.aabb.lowerLeft.y;
+        }
+
+        this.aabb.add(push);
     }
 }
